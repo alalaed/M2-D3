d58f860b9bc93b930b6a2b12eb0039cab18df8d1
Ali Alaeddine <a.alaeddine@live.com>
2021-12-15 14:44:49 +0100

    ex 2

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+const genres = ["HipHop", "Rap", "Jazz", "Alt.Rock", "RnB", "Pop"]
+
+
+
+
+let list = document.createElement("ul")
+let bodyContainer= document.querySelector("body")
+bodyContainer.appendChild(list)
+
+for( i=0; i<genres.length; i++){
+    let item = document.createElement("li")
+    item.innerText=genres[i]
+    list.appendChild(item)
+}
+
+
+function watchColorPicker(event) {
+    document.querySelectorAll("p").forEach(function(p) {
+      p.style.color = event.target.value;
+    });
+  }
+
+
+let colorPicker = function(){
+    let x = clientX 
+    let y = clientY
+    let object = document.elementFromPoint(x, y)
+    let color = object.classList.color
+    return color
+}
+
+let listItems = []
+let items = document.getElementsByTagName("li")
+for (i=0; i<items.length; i++){
+listItems.push(items[i])
+}
+
+
+
+
+
+
+
